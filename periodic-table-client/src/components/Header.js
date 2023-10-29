985fe7f9ffaf5c118fdf1e4a41fd2adbba29506f
Piero Valencia <piero_v@outlook.es>
2023-10-29 14:26:57 -0500

    Se agregó una animación para la sección del elemento seleccionado. Se agregó un icono al costado del título.

--- periodic-table-client/src/components/Header.js
+++ periodic-table-client/src/components/Header.js
@@ -1,10 +1,17 @@
 import './Header.css';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Header = () => {
 	return (
 		<Container className="Header">
-			<h1>Periodic Table</h1>
+			<Row>
+				<Col sm={1}>
+					<img id='img1' src='chemistry.png' />
+				</Col>
+				<Col sm={10}>
+					<h1>Periodic Table</h1>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
