4226d65440e0f02c138c19acbb5cf971cb4becfc
Piero Valencia <43690060+PieroValencia4@users.noreply.github.com>
2024-02-10 10:07:03 -0500

    WS: Se ha creado un archivo de conexión hacia una db en MongoDB

---
+++ periodic-table-ws/dbconnection.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const uri = "mongodb://localhost:27017"
